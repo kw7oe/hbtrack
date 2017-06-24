d8ceef687586e19fa5e620b13591fb302b62d90c
kw7oe <choongkwern@hotmail.com>
2017-06-25 00:06:55 +0800

    Finish up basic habit tracker

--- journal.rb
+++ journal.rb
@@ -1,10 +1,11 @@
 require_relative 'lib/habit_tracker'
+require_relative 'lib/parser'
 module Journal
-  def self.run
-    puts 'Running...'
-    @habit_tracker = HabitTracker.new('test.habit')
-    puts @habit_tracker.habits
-  end
+
+  def self.run(file)
+    parser = Parser.new(file)
+    habit_tracker = HabitTracker.new(parser.habits)
+  end  
 end
 
-Journal.run
+Journal.run(ARGV[0])
--- lib/habit_tracker.rb
+++ lib/habit_tracker.rb
@@ -3,28 +3,17 @@ require_relative 'habit'
 class HabitTracker
   attr_reader :habits
 
-  def initialize(file)
-    @raw_input = []
+  def initialize(habits)
     @habits = []
-    @input = []
-    File.open(file, 'r') do |f|
-      f.each_line do |line|
-        @raw_input << line
-      end
-    end
-    parse
-    generate
+    generate_habit(habits)
   end
 
-  def generate
-    @input.each_with_index do |data, index|
+  private
+  def generate_habit(habits)
+    habits.each_with_index do |data, index|
       @habits << Habit.new(index, data)
     end
   end
 
-  def parse
-    @input = @raw_input.map do |data|
-      data.lstrip.chomp
-    end
-  end
+  
 end
