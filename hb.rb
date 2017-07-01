a0d52186a432bde23c0665beaabb9cbcf8ea90fe
kw7oe <choongkwern@hotmail.com>
2017-07-01 22:42:14 +0800

    Remove requiring habit_tracker

--- hb.rb
+++ hb.rb
@@ -1,12 +1,11 @@
 # frozen_string_literal: true
 
-require_relative 'lib/habit_tracker'
 require_relative 'lib/cli'
 
 # CLI for the application
 module Hb
   def self.run(args)
-    cli = CLI.new
+    cli = CLI.new("test_data")
     cli.parse_arguments(args)
   end
 end
