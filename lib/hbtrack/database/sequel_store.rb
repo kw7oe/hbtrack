29a9e731d9afd7441b89af02e5ee54d61344c9f6
kw7oe <choongkwern@hotmail.com>
2018-01-09 10:22:47 +0800

    Add in basic DB operation of Habit and Entry witht spec

--- lib/hbtrack/database/sequel_store.rb
+++ lib/hbtrack/database/sequel_store.rb
@@ -2,35 +2,84 @@
 
 module Hbtrack
   module Database
-    module SequelStore
-      extend self
+    class SequelStore
       require 'sequel'
 
-      def start(path: 'sqlite://hbtrack.db')
-        db = connect(path)
-        initialize(db)
+      # Start DB connection
+      def self.start(name: 'hbtrack.db')
+        SequelStore.new(name)
       end
 
-      def connect(path)
-        Sequel.connect(path)
+      attr_reader :db
+      def initialize(name)
+        @db = Sequel.sqlite(name)
+        create_table?
       end
 
-      def initialize(db)
+      # Data Abstraction for Habit
+      # Habit = Struct.new(:id, :title, :page, :display_order)
 
+      # Add a habit
+      # TODO: Add error handling for existed habit
+      def add_habit(habit)
+        habits.insert(
+          title: habit.title,
+          page: habit.page,
+          display_order: habit.display_order
+        )
+      end
+
+      # Get habit by id
+      def get_habit(id)
+        habits.filter(id: id).first
+      end
+
+      # Data Abstrack for Entry
+      # Entry = Struct.new(:timestamp, :type, :habit_id)
+
+      # Add a entry of a habit
+      def add_entry_of(habit_id, entry)
+        entries.insert(
+          timestamp: entry.timestamp,
+          type: entry.type,
+          habit_id: habit_id
+        )
+      end
+
+      # Get all entries of a habit
+      def get_entries_of(habit_id)
+        entries.where(habit_id: habit_id)
+      end
+
+      private
+      # Create Habits and Entries table if doesn't exist
+      def create_table?
         db.create_table? :habits do
           primary_key :id
-          String :name
+          String :title
           Integer :page
           Integer :display_order
         end
 
         db.create_table? :entries do
           primary_key :id
+          String :type
           DateTime :timestamp
           foreign_key :habit_id, :habits
         end
       end
 
+      # Get Habits dataset
+      def habits
+        return @habits if @habit
+        @habits = db[:habits]
+      end
+
+      # Get Entries dataset
+      def entries
+        return @entries if @entries
+        @entries = db[:entries]
+      end
     end
   end
 end
---
+++ spec/database/sequel_store_spec.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe Hbtrack::Database::SequelStore do
+  before :each do
+    @store = Hbtrack::Database::SequelStore.start(name: 'test.db')
+  end
+
+  after :each do
+    File.delete('test.db')
+  end
+
+  Habit = Struct.new(:title, :page, :display_order)
+  Entry = Struct.new(:timestamp, :type)
+
+  describe 'habits' do
+    it 'should be able to add habit' do
+      struct = Habit.new("workout", 1, 1)
+      @store.add_habit(struct)
+
+      habit = @store.get_habit(1)
+
+      expect(habit[:title]).to eq "workout"
+    end
+  end
+
+  describe 'entries' do
+    let(:habit) do
+      struct = Habit.new("workout", 1, 1)
+      @store.add_habit(struct)
+      @store.get_habit(1)
+    end
+
+    it 'should be able to add entries for a habit' do
+      struct = Entry.new('2017-02-26T12:24:16+08:00', 'partially_compleed')
+      id = habit[:id]
+      @store.add_entry_of(id, struct)
+      entries = @store.get_entries_of(id)
+
+      expect(entries.count).to eq 1
+    end
+  end
+
+end
